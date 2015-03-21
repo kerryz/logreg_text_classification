5e813fc0b8b4838c97fd44de2d75246a712f0111
kerryz <kerryzhang92@gmail.com>
2015-03-22 02:13:00 +0800

    preprocessing.py: well commented now, added optional cli arguments

--- preprocessing.py
+++ preprocessing.py
@@ -1,31 +1,179 @@
+# -*- coding: utf-8 -*-
+"""
+preprocessing.py
+----------------
+Preprocesses the documents and creates feature files that we later can train our system with.
+
+To run
+------
+python preprocessing.py [data_root_dir, [output_dir, [class0, [class1]]]]
+
+Defaults:
+    data_root_dir = './given_data/data 1/'
+    output_dir = './data/'
+    class0 = 'baseball'
+    class1 = 'hockey'
+
+Assumes the following directory structure for `data_root_dir`:
+    data_root_dir
+    |-- s1
+        |-- class0
+            |-- file101
+            |-- file102
+            |-- ...
+        |-- class1
+            |-- file111
+            |-- file112
+            |-- ...
+    |-- s2
+        |-- class0
+            |-- file201
+            |-- file202
+            |-- ...
+        |-- class1
+            |-- file211
+            |-- file212
+            |-- ...
+    |-- ...
+
+Outputs
+-------
+`output_dir/si_features`
+    outputs one feature file per subset.
+    examples: `./data/s1_features`, `./data/s2_features`, etc.
+    file content format: <label> <feature1>:<value1> <feature2>:<value2>
+        where <label> == 0 or 1 since we're using logistic regression
+        example: "0 22199:0.0 13448:0.00148313739872 15346:7.97308993901e-05"
+`output_dir/word2id.json`
+    file content format {'word': word_id}
+`output_dir/word2documents.json`
+    file content format {'word': nr_of_documents_it_appears_in}
+"""
+
 from __future__ import division
-import os
+import os, sys, errno
 import nltk
 import json
 import math
 
 
-def main():
-    #generate_word_files('./given_data/data 1/', './data/', 'baseball', 'hockey')
-    genereate_feature_files('./given_data/data 1/', './data/', ('baseball', 'hockey'),
-                            1989, './data/word2documents.json', './data/word2id.json')
+def_data_root_dir = './given_data/data 1/'
+def_output_dir = './data/'
+def_class0 = 'baseball'
+def_class1 = 'hockey'
+
+
+def main(
+        data_root_dir=def_data_root_dir,
+        output_dir=def_output_dir,
+        (class0, class1)=(def_class0, def_class1)
+        ):
+    print "Analyzing documents..."
+    # write word2id and word2documents to disk as as json files
+    generate_word_files(data_root_dir, output_dir)
+
+    word2id_path = os.path.join(output_dir, 'word2id.json')
+    word2documents_path = os.path.join(output_dir, 'word2documents.json')
+    # write one feature file per subset to disk
+    genereate_feature_files(
+        data_root_dir, output_dir, (class0, class1),
+        word2id_path, word2documents_path
+    )
+
+
+def generate_word_files(data_root_dir, output_dir):
     """
-    document_count, term_in_documents_dict = get_idf_data('./given_data/data 1')
-    counter = 0
-    sorted_terms = sorted(term_in_documents_dict.items(), key=operator.itemgetter(1))
-    for (k, v) in sorted_terms:
-        print k, ": ", v
-        counter += 1
-    print '-' * 20
-    print "document_count", document_count
+    Writes word2id and word2documents as json files.
+
+    Parameters
+    ----------
+    data_root_dir : string
+        path to the root directory containing all the training data (documents)
+        every file in the directory and its subdirectory will be scanned
+    output_dir : string
+        path to the directory where `word2id.json` and `word2documents.json` will be stored
+
+    document_count : int
+        number of documents (i.e. non-hidden files) in `data_root_dir` and all its subdirectories
+
+    Outputs
+    -------
+    `output_dir/word2id.json`
+        file content format {'word': word_id}
+    `output_dir/word2documents.json`
+        file content format {'word': nr_of_documents_it_appears_in}
     """
+    # get document count and dict {term : nr of documents it appears in}
+    document_count, word2documents = get_idf_data(data_root_dir)
+    # create a word -> id mapping
+    word2id = {}
+    for i, word in enumerate(word2documents.keys()):
+        word2id[word] = i
+    # create `output_dir` if it does not exist
+    if not os.path.exists(output_dir):
+        mkdir_p(output_dir)
+    # write word -> id mapping to file in json format
+    with open(os.path.join(output_dir, 'word2id.json'), 'w') as word2id_json_file:
+        json.dump(word2id, word2id_json_file)
+    # write word -> nr_of_documents_it_appears_in in json format
+    with open(os.path.join(output_dir, 'word2documents.json'), 'w') as outfile:
+        json.dump(word2documents, outfile)
+    return document_count
 
 
 def genereate_feature_files(
         data_root_dir, output_dir, (class0, class1),
-        document_count, word2documents, word2id):
+        word2id, word2documents):
     """
+    Generates one feature file per subset, which containins the features of each subset.
+    These feature files are saved to disk as
+    `output_dir/s1_features`, `output_dir/s2_features`, etc.
 
+    Parameters
+    ----------
+    data_root_dir : string
+        path to the root folder containing all the subsets and files
+        directory structure:
+            data_root_dir
+            |-- s1
+                |-- class0
+                    |-- file101
+                    |-- file102
+                    |-- ...
+                |-- class1
+                    |-- file111
+                    |-- file112
+                    |-- ...
+            |-- s2
+                |-- class0
+                    |-- file201
+                    |-- file202
+                    |-- ...
+                |-- class1
+                    |-- file211
+                    |-- file212
+                    |-- ...
+            |-- ...
+
+        output_dir : string
+            path to the directory where the feature files will be stored
+        (class0, class1) : (string, string)
+            names of the directories that contain files for class 0 and class 1 respectively
+        word2id: string or dict
+            if dict: format {'word': word_id}
+            elif string: path to a json file with above format
+        word2documents : string or dict
+            if dict: format {'word': nr_of_documents_it_appears_in}
+            elif string: path to a json file with above format
+
+    Outputs
+    -------
+    `output_dir/si_features`
+        outputs one feature file per subset.
+        example: `./data/s1_features`
+        format: <label> <feature1>:<value1> <feature2>:<value2>
+            where <label> == 0 or 1 since we're using logistic regression
+            example: "0 22199:0.0 13448:0.00148313739872 15346:7.97308993901e-05"
     """
 
     # check if input argument is dict, or path to json file and load it
@@ -35,6 +183,16 @@ def genereate_feature_files(
     if isinstance(word2documents, basestring):
         with open(word2documents) as json_file:
             word2documents = json.load(json_file)
+
+    # count number of documents in `data_root_dir`
+    document_count = sum([len(
+        filter(lambda f: f[0] != '.', files))  # do not count hidden files
+        for _, _, files in os.walk(data_root_dir)
+    ])
+
+    # create `output_dir` if it does not exist
+    if not os.path.exists(output_dir):
+        mkdir_p(output_dir)
 
     # get subset directory names
     _, subsets, _ = next(os.walk(data_root_dir))
@@ -64,79 +222,23 @@ def genereate_feature_files(
                 document_features = [label]
                 # get data for calculating tf-idf of each word in document
                 word_count_in_document_dict = get_word_counts_in_document(document_path)
-
-                #######################################################################
-                print "words in document", len(word_count_in_document_dict.keys())
-                if document_path == './given_data/data 1/s1/baseball/100521':
-                    print "nr of documents:", len(documents)
-                    print document_path
-                    print len(word_count_in_document_dict.keys())
-                    ii = 0
-                    for k, v in word_count_in_document_dict.iteritems():
-                        print k, v
-                        if ii > 20:
-                            print "etc etc..."
-                            break
-                        ii += 1
-                #######################################################################
-
                 total_words_in_document = sum(word_count_in_document_dict.values())
+
                 # calculate tf-idf for each word in document
                 for word, count in word_count_in_document_dict.iteritems():
                     tf_idf = (count / total_words_in_document) \
                         * math.log(document_count / (word2documents.get(word, 0) + 1))
                     document_features.append(str(word2id[word]) + ":" + str(tf_idf))
-                    """
-                    ##########################################################################
-                    print '-' * 30
-                    print "word:", word
-                    print "count:                 ", count
-                    print "total_words_in_document", total_words_in_document
-                    print "document_count:        ", document_count
-                    print "word2documents_count:  ", word2documents.get(word, 0)
-                    print "document_count / word2doc", document_count / (word2documents.get(word, 0) + 1)
-                    print "tf:  ", (count / total_words_in_document)
-                    print "idf:", math.log(document_count / (word2documents.get(word, 0) + 1))
-                    print "tfidf:", tf_idf
-                    ##########################################################################
-                    """
                 # after calculating tf-idf for each word in this document
                 # save them as one line string
                 document_features_str = " ".join(document_features)
                 # add all the tf_idf values of every word in the document to subset_features
                 subset_features.append(document_features_str)
-                ########################################################################
-                # Test
-                #print document_features
-                #print len(document_features)
-                #return
-                ########################################################################
+
         # write feature values of this subset to file
         with open(os.path.join(output_dir, s_i + "_features"), 'w') as si_feature_file:
             for document_features in subset_features:
                 si_feature_file.write(document_features + "\n")
-                ########################################################################
-                # Test
-                #print document_features
-                #break
-                ########################################################################
-
-
-def generate_word_files(data_root_dir, output_dir, class0, class1):
-    """
-    Writes word2id and word2documents as json files.
-    """
-    # get document count and dict {term : nr of documents it appears in}
-    document_count, word2documents = get_idf_data(data_root_dir)
-    # create a word -> id mapping
-    word2id = {}
-    for i, word in enumerate(word2documents.keys()):
-        word2id[word] = i
-    # write word -> id mapping to file in json format
-    with open(os.path.join(output_dir, 'word2id.json'), 'w') as word2id_json_file:
-        json.dump(word2id, word2id_json_file)
-    with open(os.path.join(output_dir, 'word2documents.json'), 'w') as outfile:
-        json.dump(word2documents, outfile)
 
 
 def get_idf_data(data_root_dir):
@@ -146,7 +248,8 @@ def get_idf_data(data_root_dir):
     Parameters
     ----------
     data_root_dir : string
-        path to the root direcory containing all the training data (documents)
+        path to the root directory containing all the training data (documents)
+        every file in the directory and its subdirectory will be scanned
 
     Returns
     -------
@@ -231,5 +334,32 @@ def get_word_counts_in_document(document_path):
     return word_counts_dict
 
 
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
+
 if __name__ == '__main__':
-    main()
+    # set default values
+    data_root_dir = def_data_root_dir
+    output_dir = def_output_dir
+    class0 = def_class0
+    class1 = def_class1
+
+    # use command line arguments if provided
+    arg_count = len(sys.argv)
+    if arg_count > 1:
+        data_root_dir = sys.argv[1]
+    if arg_count > 2:
+        output_dir = sys.argv[2]
+    if arg_count > 3:
+        class0 = sys.argv[3]
+    if arg_count > 4:
+        class1 = sys.argv[4]
+
+    main(data_root_dir, output_dir, (class0, class1))
